ac5dae9f1df4bd039c0d4f4e2b194deaba9db57a
Sharon Wag <sharon.ran.wang@gmail.com>
2015-10-24 14:48:02 -0400

    merged again

--- app/controllers/travellers_controller.rb
+++ app/controllers/travellers_controller.rb
@@ -1,6 +1,14 @@
 class TravellersController < ApplicationController
 
 	def index 
+		@interests = ["sangria", "pinot noir", "whiskey"];
+		@locations = ["barcelona", "prague", "deadsea"];
+		# @startdate = [];
+		# @enddate = [];
+
+
+
+		# check to see if tStartdate is =< eStartdate, if tEnddate is => eEndddate  
 
 	end
 
