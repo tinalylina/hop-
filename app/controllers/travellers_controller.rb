f1c17bb41ca9adc9f9eeda767c6b3e6d196b4f15
Sharon Wag <sharon.ran.wang@gmail.com>
2015-10-24 13:58:32 -0400

    first commit

---
+++ app/controllers/travellers_controller.rb
@@ -0,0 +1,14 @@
+class TravellersController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+
+	def index 
+		
+	end
+
+	def results 
+
+	end
+
+  protect_from_forgery with: :exception
+end
